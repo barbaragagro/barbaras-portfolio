d74e16d660aa5daa405c8a1b6c886ac4fe2f4041
barbaragagro <barbaragagro1@gmail.com>
2022-11-11 18:30:42 +0100

    first commit

---
+++ barbaras-portfolio/src/container/contactMe/ContactMe.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+ import axios from "axios";
+ import { toast } from "react-toastify";
+
+import imgBack from "../../../src/assets/images/mailz1.jpeg";
+import load1 from "../../../src/assets/images/load2.gif";
+import ScreenHeading from "../../utilities/screenHeading/ScreenHeading";
+
+import Footer from "../../container/footer/Footer";
+import "./ContactMe.css";
+
+export default function ContactMe(props) {
+
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  console.log(name);
+  const submitForm = async (e) => {
+    e.preventDefault();
+    try {
+      let data = {
+        name,
+        email,
+        message,
+      };
+      setBool(true);
+     const res = await axios.post(`/contact`, data);
+      if (name.length === 0 || email.length === 0 || message.length === 0) {
+        setBanner(res.data.msg);
+        toast.error(res.data.msg);
+        setBool(false);
+      } else if (res.status === 200) {
+        setBanner(res.data.msg);
+        toast.success(res.data.msg);
+        setBool(false);
+
+        setName("");
+        setEmail("");
+        setMessage("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="main-container fade-in" id={props.id || ""}>
+      <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col">
+          <h2 className="title">
+            Get In Touch 📧
+          </h2>{" "}
+
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            <img src={imgBack} alt="not found" />
+          </div>
+          <form onSubmit={submitForm}>
+            <p>{banner}</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" onChange={handleName} value={name} />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" onChange={handleEmail} value={email} />
+
+            <label htmlFor="message">Message</label>
+            <textarea type="text" onChange={handleMessage} value={message} />
+
+            <div className="send-btn">
+              <button type="submit">
+                send
+                <i className="fa fa-paper-plane" />
+                {bool ? (
+                  <b className="load">
+                    <img src={load1} alt="not responding" />
+                  </b>
+                ) : (
+                  ""
+                )}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ barbaras-portfolio/src/utilities/CommonUtils.js
@@ -0,0 +1,31 @@
+import Home from "../container/home/Home";
+import AboutMe from "../container/aboutMe/AboutMe";
+import Resume from "../container/resume/Resume";
+ import ContactMe from "../container/contactMe/ContactMe";
+
+export const TOTAL_SCREENS =[
+    {
+        screen_name: "Home",
+        component: Home,
+    },
+    {
+        screen_name : "AboutMe",
+        component: AboutMe,
+    },
+    {
+        screen_name : "Resume",
+        component: Resume,
+    },
+     {
+         screen_name : "ContactMe",
+         component: ContactMe,
+     },
+]
+
+export const GET_SCREEN_INDEX = (screen_name) => {
+    if(!screen_name) {return -1};
+    for(let i=0; i< TOTAL_SCREENS.length; i++){
+        if(TOTAL_SCREENS[i].screen_name===screen_name) return i;
+    }
+    return -1
+};
